f6c3ee4c548e9c96714b9a8e488265e00ef4175f
romanoen <roman@oelfken.net>
2025-01-27 22:10:21 +0100

    minor improvement

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import yfinance as yf
 import numpy as np
+from datetime import datetime
 from plot import plot_simulations, plot_histogram_with_normal
 from compute import monte_carlo_simulation
 
@@ -13,7 +14,7 @@ class StockAnalysisApp:
         self.start_date = "2010-01-01"
         self.end_date = "2025-01-01"
         self.stock_data = None
-        self.T = 10000 #Anzahl Simulationen
+        self.T = 1000 #Anzahl Simulationen
         self.N = 252 #Tage zu simulieren
         self.initialPrice = None
         self.mu = None
@@ -21,7 +22,7 @@ class StockAnalysisApp:
 
     def get_user_input(self):
         """Create input widgets for the user to specify stock ticker and date range."""
-        st.title("Stock Analysis and Visualization App")
+        st.title("Portfolio Risk Analysis")
 
         # Create a 3-column layout for ticker input, date range, and the Analyze button
         col1, col2, col3 = st.columns(3)
@@ -33,28 +34,56 @@ class StockAnalysisApp:
         # Dropdown and button in the second column
         with col2:
             col21, col22, col23, col24, col25 = st.columns(5)
-            # Create a dropdown with options
             with col21:
-                time_range = st.selectbox("Hist. Pricing:", ["1 Y", "5 Y", "10 Y"])
-
-                # Set the start and end date based on the selected option
-                if time_range == "1 Y":
-                    self.start_date = "2024-01-01"
-                    self.end_date = "2025-01-01"
-                elif time_range == "5 Y":
-                    self.start_date = "2020-01-01"
-                    self.end_date = "2025-01-01"
-                elif time_range == "10 Y":
-                    self.start_date = "2010-01-01"
-                    self.end_date = "2025-01-01"
-            
-            with col22:
-                col22.markdown("<div style='width: 1px; height: 28px'></div>", unsafe_allow_html=True)
+                col21.markdown("<div style='width: 1px; height: 28px'></div>", unsafe_allow_html=True)
                 # Add the "Analyze Stock" button
                 analyze_button_clicked = st.button("Go")
 
             return analyze_button_clicked
+    def sidebar(self):
+        time_options = ["1 Month", "1 Year", "5 Years", "10 Years"]
+        st.sidebar.divider()
+        # Create the select slider
+        selected_time = st.sidebar.select_slider(
+            "Number of years providing historical returns So,  10 Years means from 2015 to 2025",
+            options=time_options,  # Discrete options
+            value="10 Years"  # Default value
+        )
 
+        # Set start and end dates based on the selected option
+        if selected_time == "1 Month":
+            self.start_date = "2024-01-12"
+            self.end_date = "2025-01-01"
+        elif selected_time == "1 Year":
+            self.start_date = "2024-01-01"
+            self.end_date = "2025-01-01"
+        elif selected_time == "5 Years":
+            self.start_date = "2020-01-01"
+            self.end_date = "2025-01-01"
+        elif selected_time == "10 Years":
+            self.start_date = "2010-01-01"
+            self.end_date = "2025-01-01"
+
+        st.sidebar.divider()
+
+        t_simulation_runs = ["10", "100", "1,000", "10,000", "100,000"]
+
+        selected_time = st.sidebar.select_slider(
+            "Number of Simulation Runs (Calculated Paths)",
+            options=t_simulation_runs,  # Discrete options
+            value="1,000"  # Default value
+        )
+
+        if selected_time == "10":
+            self.T = 10
+        elif selected_time == "100":
+            self.T = 100
+        elif selected_time == "1,000":
+            self.T = 1000
+        elif selected_time == "10,000":
+            self.T = 10000
+        elif selected_time == "100,000":
+            self.T = 100000
 
 
     def fetch_stock_data(self):
@@ -94,7 +123,7 @@ class StockAnalysisApp:
         
     def plotMC(self):
 
-        matrix = monte_carlo_simulation(self.N, self.T, 100, self.mu, self.sigma)
+        matrix = monte_carlo_simulation(self.N, self.T, self.initialPrice, self.mu, self.sigma)
 
         fig = plot_simulations(matrix, self.N, self.T)
 
@@ -102,6 +131,8 @@ class StockAnalysisApp:
 
     def run(self):
         """Run the Streamlit app."""
+        st.sidebar.title("Configure Simulation")
+        self.sidebar()
         analyze_button_clicked = self.get_user_input()
 
         if analyze_button_clicked:
